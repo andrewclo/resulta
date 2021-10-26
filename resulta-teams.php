6830a67790c7488026e53a4e0b03bf4b7bceaf9e
Andrew Clough <andrewclo@gmail.com>
2021-10-25 17:28:05 -0700

    NFL Teams Plugin

---
+++ resulta-teams.php
@@ -0,0 +1,60 @@
+<?php
+/**
+* Plugin Name: Resulta NFL Team Sorter
+* Plugin URI: https://www.andrewclough.com
+* Description: This is a Resulta plugin using AlpineJS
+* Version: 1.0
+* Author: Andrew Clough
+**/
+
+function build_table() { ?>
+<script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
+<?php
+$request = wp_remote_get( 'http://delivery.chalk247.com/team_list/NFL.JSON?api_key=74db8efa2a6db279393b433d97c2bc843f8e32b0' );
+
+if( is_wp_error( $request ) ) {
+	return false; 
+}
+
+$body = wp_remote_retrieve_body( $request );
+$data = json_decode( $body );
+
+if( ! empty( $data ) ) { ?>
+
+
+<div x-data="data()" class="resulta-teams">
+<table>
+	<thead>
+		<th @click="sortByColumn">City</th>
+		<th @click="sortByColumn">Name</th>
+		<th @click="sortByColumn">Conference</th>
+		<th @click="sortByColumn">Division</th>
+	</thead>	
+	<tbody x-ref="tbody">
+<?php
+	foreach( $data->results->data->team as $team ) {
+		echo '<tr>';
+			echo  '<td>'.$team->display_name.'</td>';
+			echo  '<td>'.$team->nickname.'</td>';
+			echo  '<td>'.$team->conference.'</td>';
+			echo  '<td>'.$team->division.'</td>';
+		echo '</tr>';
+	}
+	echo '</tbody></table>';
+}
+?>
+</div>
+
+<?php
+}
+
+function resulta_teams_function() {
+    // wp_enqueue_script( 'alpinejs', 'https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js' );
+    wp_enqueue_script( 'teamsort',plugin_dir_url( __FILE__ ) . 'team-sort.js' );
+    wp_enqueue_style( 'style',  plugin_dir_url( __FILE__ ) . '/assets/resulta-team.css');
+    build_table();
+}
+
+add_shortcode('resulta-teams', 'resulta_teams_function');
+
+?>
---
+++ team-sort.js
@@ -0,0 +1,54 @@
+//Table Sorting with Alpine.js from https://codepen.io/ryangjchandler/pen/WNQQKeR
+
+function data() {
+    return {
+        sortBy: "",
+        sortAsc: false,
+        active: false,
+        sortByColumn($event) {
+            
+            if (this.sortBy === $event.target.innerText) {
+                if (this.sortAsc) {
+                    this.sortBy = "";
+                    this.sortAsc = false;
+                } else {
+                    this.sortAsc = !this.sortAsc;
+                }
+            } else {
+                this.sortBy = $event.target.innerText;
+            }
+
+            let rows = this.getTableRows()
+                .sort(
+                    this.sortCallback(
+                        Array.from($event.target.parentNode.children).indexOf(
+                            $event.target
+                        )
+                    )
+                )
+                .forEach((tr) => {
+                    this.$refs.tbody.appendChild(tr);
+                });
+        },
+        getTableRows() {
+            return Array.from(this.$refs.tbody.querySelectorAll("tr"));
+        },
+        getCellValue(row, index) {
+            return row.children[index].innerText;
+        },
+        sortCallback(index) {
+            return (a, b) =>
+                ((row1, row2) => {
+                    return row1 !== "" &&
+                        row2 !== "" &&
+                        !isNaN(row1) &&
+                        !isNaN(row2)
+                        ? row1 - row2
+                        : row1.toString().localeCompare(row2);
+                })(
+                    this.getCellValue(this.sortAsc ? a : b, index),
+                    this.getCellValue(this.sortAsc ? b : a, index)
+                );
+        }
+    };
+}
